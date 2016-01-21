acc3f073971b3e5cd8ca51cb502f0f83de9cacec
Artem <q3pp@yandex.ru>
2016-01-21 14:26:18 +0300

    start refactoring

---
+++ RailsProject/Diplom/app/controllers/hi_controller.rb
@@ -0,0 +1,5 @@
+class HiController < ApplicationController
+def home
+
+end
+end
---
+++ RailsProject/Diplom/test/controllers/hi_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HiControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
